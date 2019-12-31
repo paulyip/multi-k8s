acfae4d13f94a70aabfaa0ffc5ea85df121f301e
Paul <paulyip@redkoda.com>
2019-12-31 17:05:55 +0800

    - make change

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker build -t paulyip34/multi-client:latest -t paulyip34/multi-clients:$SHA -f ./client/Dockerfile ./client
+docker build -t paulyip34/multi-client:latest -t paulyip34/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t paulyip34/multi-server:latest -t paulyip34/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t paulyip34/multi-worker:latest -t paulyip34/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push paulyip34/multi-client:latest
@@ -9,6 +9,6 @@ docker push paulyip34/multi-server:$SHA
 docker push paulyip34/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=paulyip34/multi-server:$SHA
-kubectl set image deployments/client-deployment server=paulyip34/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=paulyip34/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=paulyip34/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=paulyip34/multi-worker:$SHA
 
